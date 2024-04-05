8375370f5c498ae84b28e43a8591d4d3aeddf31c
WTurner3209 <winston0932@gmail.com>
2024-04-05 15:00:28 -0400

    vp3 15 unknown error

--- src/main/java/org/wecancodeit/Controllers/ShelterController.java
+++ src/main/java/org/wecancodeit/Controllers/ShelterController.java
@@ -13,7 +13,7 @@ import org.wecancodeit.Services.ShelterService;
 
 import jakarta.annotation.Resource;
 
-@RestController 
+@RestController
 @RequestMapping("/api/v1/shelters/")
 public class ShelterController {
 
@@ -23,36 +23,42 @@ public class ShelterController {
     public ShelterController(ShelterService shelterService) {
         this.shelterService = shelterService;
     }
-     @GetMapping
+
+    @GetMapping
     public Iterable<ShelterModel> getAllShelters() {
         return shelterService.findAll();
     }
+
     @GetMapping("{id}")
     public ShelterModel getShelterId(@PathVariable Long id) {
         return shelterService.findById(id);
     }
+
     @GetMapping("name/{name}")
     public Iterable<ShelterModel> getName(@PathVariable String name) {
         return shelterService.findByName(name);
     }
+
     @GetMapping("zip/{zip}")
     public Iterable<ShelterModel> getZip(@PathVariable String zip) {
         return shelterService.findByZip(zip);
     }
+
     @DeleteMapping("{id}")
     public boolean deleteShelter(@PathVariable Long id) {
         return shelterService.deleteShelter(id);
     }
+
     @PostMapping
     public ShelterModel addShelter(@RequestBody ShelterModel shelterModel) {
 
         return shelterService.saveShelter(shelterModel);
     }
+
     @PutMapping
     public ShelterModel saveShelter(@RequestBody ShelterModel shelterModel) {
 
         return shelterService.saveShelter(shelterModel);
     }
 
-    
 }
